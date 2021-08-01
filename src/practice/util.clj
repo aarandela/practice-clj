06900772772bf268a8a714d33c79d0ccbbae9988
Audy Arandela <audyarandela@gmail.com>
2021-07-31 19:39:58 -0500

    Slurp and tests

---
+++ src/practice/util.clj
@@ -0,0 +1,31 @@
+(ns practice.util
+  (:require
+   [clojure.string :as string]))
+
+(defn format-dob
+  [dob]
+  {:pre [(string/includes? dob "/")]} ;; simple validation
+  (->> dob
+       (.parse (java.text.SimpleDateFormat. "MM/dd/yyyy"))
+       (.format (java.text.SimpleDateFormat. "M/dd/yyyy"))))
+
+(defn format-and-replace-dob 
+  "Takes in a collection and replaces dob with the formatted dob"
+  [coll]
+  (map (fn [e]
+         (let [old-dob (:dob e)] 
+           (assoc e :dob (format-dob old-dob))))
+       coll))
+
+(defn convert-to-map
+  "Takes in a sequence of vectors and converts to a sequence of maps assuming values are what they are supposed to be"
+  [coll]
+  (map #(apply assoc {} 
+               (interleave [:last-name :first-name :email :fav-color :dob] %)) coll))
+
+(comment 
+  (format-dob "05/05/1098")
+  (format-dob "5/5/2000")
+  (def converted (convert-to-map '(["last" "first" "email" "color" "02/03/2000"])))
+  (format-and-replace-dob converted))
+         
--- test/practice/core_test.clj
+++ test/practice/core_test.clj
@@ -1,7 +1,39 @@
 (ns practice.core-test
-  (:require [clojure.test :refer :all]
-            [practice.core :refer :all]))
+  (:require [clojure.test :refer [deftest is testing run-all-tests]]
+            [practice.core :refer [output-1-comparator determine-and-format]]
+            [practice.util :refer [format-dob convert-to-map format-and-replace-dob]]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "test"
+    (is (= 1 1))))
+
+(def data-model '({:last-name "last" 
+                   :first-name "first" 
+                   :email "email" 
+                   :fav-color "color" 
+                   :dob "02/03/2000"}))
+
+(deftest utils
+  (testing "util functions"
+    (is (= (format-dob "01/20/1999") "1/20/1999"))
+    (is (thrown? java.lang.AssertionError 
+                 (format-dob "invalid")))
+    
+    (is (= (convert-to-map '(["last" "first" "email" "color" "02/03/2000"]))
+           data-model)) 
+    (is (not (seq (convert-to-map nil))))
+    
+    (is (= (format-and-replace-dob data-model) 
+           '({:last-name "last", :first-name "first", :email "email", :fav-color "color", :dob "2/03/2000"})))))
+
+(deftest core
+  (testing "functions in core ns"
+    (is (= (output-1-comparator ["A" "A"] ["A" "B"])
+           -1))
+    (is (= (output-1-comparator ["A" "A"] ["B" "B"])
+           1))
+    (is (thrown? java.lang.AssertionError
+                 (determine-and-format nil)))))
+
+(comment
+  (run-all-tests))
